1603e32dd6dc647f287aca550260bf788ef68885
AtillaTahak <atillatahaa@gmail.com>
2024-05-23 23:54:24 +0300

    chore: Add welcome controller, views, and helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,56 @@
 class ArticlesController < ApplicationController
-  def index
-  end
+	before_action :set_article, only: %i[show edit update destroy]
 
-  def create
-  end
+	def index
+	  @articles = Article.all
+	end
 
-  def update
-  end
+	def show
+	end
 
-  def destroy
+	def new
+	  @article = Article.new
+	end
+
+	def edit
+	end
+
+	def create
+	  @article = Article.new(article_params)
+
+	  unless @article.content.present?
+		@article.content = 'content will be here'
+	  end
+	  if @article.save
+		redirect_to @article, notice: 'Article was successfully created.'
+	  else
+		render :new, status: :unprocessable_entity
+	  end
+	end
+
+	def update
+	  if @article.update(article_params)
+		unless @article.content.present?
+			@article.content = 'content will be here'
+		end
+		redirect_to @article, notice: 'Article was successfully updated.'
+	  else
+		render :edit, status: :unprocessable_entity
+	  end
+	end
+
+	def destroy
+	  @article.destroy
+	  redirect_to articles_path, notice: 'Article was successfully deleted.'
+	end
+
+	private
+
+	def set_article
+	  @article = Article.find(params[:id])
+	end
+
+	def article_params
+	  params.require(:article).permit(:title, :content, :status)
+	end
   end
-end
--- db/migrate/20240523201645_create_articles.rb
+++ db/migrate/20240523201645_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :content
+      t.text :content, default: 'content will be here'
       t.integer :status
 
       t.timestamps
